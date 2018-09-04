9cd0655a14fceb243ff72b27c56d1bbe9b1d92f7
Taqtile <taqtile@Gold.local>
2018-09-04 12:25:28 -0300

    Refactor import and function return

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -1,10 +1,8 @@
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 // Create a component
-const App = () => {
-    return (
-        <Text>Some Text</Text>
-    );
-}
+const App = () => <Text>Some Text</Text>;
+
 // Render it to the device
+AppRegistry.registerComponent('albums', () => App);
