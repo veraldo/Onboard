dec7fa3dac313d94e6ecfc66c5f7d345608cf5f6
veraldo <everaldogaliano@gmail.com>
2018-09-05 14:46:20 -0300

    Revert "React-native tutorial files"

--- AwesomeProject/index.js
+++
@@ -1,6 +0,0 @@
-// Import a library to help create a component
-import { AppRegistry } from 'react-native';
-import App from './App.tsx';
-
-// Render it to the device
-AppRegistry.registerComponent('AwesomeProject', () => App);
