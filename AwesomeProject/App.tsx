6a41f2a5bb71fbcec5106d561128e71fe26b14c6
Taqtile <taqtile@Gold.local>
2018-09-11 13:19:13 -0300

    Show user details on item click; refactoring

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -2,7 +2,7 @@ import { createStackNavigator } from 'react-navigation';
 import HomeScreen from './src/components/HomeScreen';
 import LoginScreen from './src/components/LoginScreen';
 import WelcomeScreen from './src/components/WelcomeScreen';
-
+import DetailsScreen from './src/components/DetailsScreen';
 
 const App = createStackNavigator(
   {
@@ -17,6 +17,10 @@ const App = createStackNavigator(
     Welcome: {
       screen: WelcomeScreen,
       headerTitle: 'Bem-vindo'
+    },
+    Details:{
+      screen: DetailsScreen,
+      header:'Details'
     }
   },
   {
